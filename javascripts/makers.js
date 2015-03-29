0ed218fdf00039f8bf45a68eb40c6a3be02fb096
Thomas Ochman <thomas@nocebo.se>
2015-03-29 21:02:51 +0200

    initial commit

---
+++ javascripts/makers.js
@@ -0,0 +1,34 @@
+var Makers   = Makers   || {},
+    document = document || {},
+    jQuery   = jQuery   || {};
+
+(function(Makers, $, document) {
+  "use strict";
+
+  Makers.init = function() {
+    $('#twitter-stream').tweetable({
+      username:   'MakersAcademy',
+      html5:      true,
+      rotate:     true,
+      replies:    false,
+      retweets:   true,
+      limit:      20,
+      time:       true,
+      onComplete: function($ul){
+        $('time').timeago();
+      }
+    });
+    $('#site_container').waypoint(Makers.revealBar);
+  };
+
+  Makers.revealBar = function(direction){
+    if(direction=="down"){
+      $('#reveal-bar').slideDown();
+    } else {
+      $('#reveal-bar').slideUp();
+    }
+  };
+
+  $(Makers.init);
+
+}(Makers, jQuery, document));
